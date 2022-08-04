3b65b5f1db5a463c08777f2055f9fbfa9d96eb85
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-04 14:37:36 -0700

    connect server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit:"30mb", extended: true }));
+app.use(express.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
+
+//mongodb.com/cloud/atlas
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(`${error} did not connect.`) );
+
+// mongoose.set('useFindAndModify', false);
