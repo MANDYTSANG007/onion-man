c0ee76b8ec9d454f95af18474398787fdd10de70
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-04 15:31:34 -0700

    add Form, Posts, and Post components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
 
 function App() {
   return (
-    <div>
-      <h1>Onion Man</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Onion Recipes</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing="3">
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
