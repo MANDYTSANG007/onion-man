3b5403a0d0cd1336826b1d8c85b7b62ce85ffebd
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-05 17:27:19 -0700

    add googleOAuth feature

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,22 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => (
-
+  <GoogleOAuthProvider clientId=
     <BrowserRouter>
       {/* <ThemeProvider theme={theme}> */}
-        <Container sx={{ bgcolor: "secondary.main" }} maxWidth="lg">
-          <Navbar />
-          <Routes>
-            <Route path="/" exact element={<Home/>} />
-            <Route path="/auth" exact element={<Auth/>} />
-          </Routes>
-        </Container>
+      <Container sx={{ bgcolor: "secondary.main" }} maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/auth" exact element={<Auth />} />
+        </Routes>
+      </Container>
       {/* </ThemeProvider> */}
     </BrowserRouter>
+  </GoogleOAuthProvider>
 );
 
 export default App;
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Input from './Input';
+import { GoogleLogin } from '@react-oauth/google';
+import Icon from './icon';
+import { useDispatch } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 
 
 const Auth = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
 
     const handleSubmit = () => {
 
@@ -20,10 +25,37 @@ const Auth = () => {
         setShowPassword((prevShowPassword) => !prevShowPassword);
     };
 
+    // Switch form feature for sign up and sign in
     const switchMode = () => {
-        setIsSignup((prevIsSignup) => !prevIsSignup );
+        setIsSignup((prevIsSignup) => !prevIsSignup);
         handleShowPassword(false);
     }
+
+    const handleCallbackResponse = async (response) => {
+        const userObject = jwt_decode(response.credential);
+        const credential = response.credential;
+        console.log(userObject);
+        try {
+            dispatch({ type: "AUTH", data: {userObject, credential}});
+
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        /* global google*/
+        google.accounts.id.initialize({
+            client_id: 
+            callback: handleCallbackResponse
+        });
+        google.accounts.id.prompt();
+    }, []);
+
+    const googleError = (error) => {
+        console.log(error);
+        console.log("Google sign in was unsuccessful. Try again later.")
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -43,11 +75,21 @@ const Auth = () => {
                         <Input name="email" label=" Email Address" handleChange={handleChange} type="email" />
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                         {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
-                        
                     </Grid>
+
                     <Button type="submit" fullWidth variant="contained" color="primary">
                         {isSignup ? "Sign Up" : "Sign In"}
                     </Button>
+
+                    <GoogleLogin
+                        render={(renderProps) => (
+                            <Button color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">Google Sign In</Button>
+                        )}
+                        onSuccess={(response) => handleCallbackResponse(response)}
+                        onError={googleError}
+                        cookiePolicy="single_host_origin"
+                    />
+
                     <Grid container justify="flex-end">
                         <Grid item>
                             <Button onClick={switchMode}>
