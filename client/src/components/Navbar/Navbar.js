83ccd34520270af4a352baf685e35b009129c9c5
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-06 14:40:23 -0700

    add logout logic

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { AppBar, Typography, Avatar, Toolbar, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 
 const Navbar = () => {
 
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+
     console.log(user);
 
     useEffect(() => {
         const credential = user?.credential;
         
         setUser(JSON.parse(localStorage.getItem('profile')));
-    }, []);
+    }, [location]);
+
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+        navigate('/');
+        setUser(null);
+    };
 
     return (
         <AppBar sx={{ bgcolor: "primary.main" }} position="static" color="inherit">
@@ -25,10 +35,10 @@ const Navbar = () => {
                     <div>
                         <Avatar alt={user.userObject.name} src={user.userObject.imageUrl}>{user.userObject.name.charAt(0)}</Avatar>
                         <Typography variant="h6">{user.userObject.name}</Typography>
-                        <Button variant="contained" color="secondary">Logout</Button>
+                        <Button variant="contained" color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ): (
-                    <Button component={Link} to="/auth" variant="contained" color="primary">Signin</Button>
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign On</Button>
                 )}
             </Toolbar>
         </AppBar>
