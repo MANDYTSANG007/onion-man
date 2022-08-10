6b051521af505997129e0c80298d5a8a95b46175
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-10 10:29:05 -0700

    fix heroku error: cannot find a required file. move all server files to root

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ app.get('/', (req, res) => {
     res.send("Onion Man API")
 });
 
-const path = require("path");
-const favicon = require("serve-favicon");
-app.use(favicon(path.join(dirname, "build", "favicon.ico")));
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("/*", function(request, response) {
-    response.sendFile(path.join(__dirname, "./client/build", "index.html"));
-})
+// const path = require("path");
+// const favicon = require("serve-favicon");
+// app.use(favicon(path.join(dirname, "build", "favicon.ico")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("/*", function(request, response) {
+//     response.sendFile(path.join(__dirname, "./client/build", "index.html"));
+// })
 
 //mongodb.com/cloud/atlas
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(process.env.PORT ||3000, () => console.log(`Server running on port: ${PORT}`)) )
+    .then(() => app.listen(PORT , () => console.log(`Server running on port: ${PORT}`)) )
     .catch((error) => console.log(`${error} did not connect.`) );
 
 // mongoose.set('useFindAndModify', false);
