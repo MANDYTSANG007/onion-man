93b20affa07682e65e85c429b438e283e17afd9e
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-09 20:57:28 -0700

    test on deployment

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get("*", function(request, response) {
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .then(() => app.listen(process.env.PORT ||3000, () => console.log(`Server running on port: ${PORT}`)) )
     .catch((error) => console.log(`${error} did not connect.`) );
 
 // mongoose.set('useFindAndModify', false);
