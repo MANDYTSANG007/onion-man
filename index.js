5f9eced79fe461a2fb91d1c744e6aec3247c8744
Mandy Tsang <mandy.tsang007@gmail.com>
2022-08-11 21:15:59 -0700

    fix tracking issue

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+// const express = require('express');
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit:"30mb", extended: true }));
+app.use(express.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Onion Man API")
+});
+
+// import path from 'path';
+// const publicPath = path.join(__dirname, "..", "public");
+// app.use(express.static(publicPath));
+// const favicon = require("serve-favicon");
+// app.use(favicon(path.join(dirname, "build", "favicon.ico")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("/*", function(request, response) {
+//     response.sendFile(path.join(__dirname, "./client/build", "index.html"));
+// })
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+// app.get("/*", function(request, response) {
+//     response.sendFile(path.join(publicPath, "/client/build", "index.html"));
+// })
+
+//mongodb.com/cloud/atlas
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(port , () => console.log(`Server running on port: ${port}`)) )
+    .catch((error) => console.log(`${error} did not connect.`) );
+
+// mongoose.set('useFindAndModify', false);
---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'test';
+
+const auth = async (req, res, next) => {
+    try {
+        const credential = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = credential.length < 500;
+
+        let decodedData;
+
+        if(credential && isCustomAuth){
+            decodedData = jwt.verify(credential, secret);
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(credential);
+            req.userId = decodedData?.sub;
+        }
+        next();
+    } catch (error){
+        console.log(error);
+    }
+}
+
+export default auth;
